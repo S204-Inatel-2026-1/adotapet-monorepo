1b3c9cc39b29464cc3416213466d1ada5a96932d
Lucas David <lucasdavidoliveira80@gmail.com>
2026-06-15 17:28:18 -0300

    chore: update environment configurations and workflows
    - Updated ignore files and setup scripts
    - Adjusted GitHub Actions workflows

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   allowCypressEnv: false,
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 90000,
 
   e2e: {
     baseUrl: 'http://localhost:3001',
